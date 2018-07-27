767f95154ba7b5c0a4eb6fd4c19365f1e3e40f0e
fun-club <alexlazariuc@outlook.com>
2018-07-27 10:13:11 +0300

    Added book model to project

---
+++ LibraryManagementSystem/Models/Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace LibraryManagementSystem.Models
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string SerialNumber { get; set; }
+        public string Author { get; set; }
+        public string Publisher { get; set; }
+    }
+}
