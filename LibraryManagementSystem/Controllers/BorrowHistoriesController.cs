907eea5ad7470baacc8d364c9df4c9461d320be7
fun-club <alexlazariuc@outlook.com>
2018-07-27 13:14:48 +0300

    Added BorrowHistoriesController with views and CRUD actions

---
+++ LibraryManagementSystem/Controllers/BorrowHistoriesController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using LibraryManagementSystem.Models;
+
+namespace LibraryManagementSystem.Controllers
+{
+    public class BorrowHistoriesController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: BorrowHistories
+        public ActionResult Index()
+        {
+            var borrowHistories = db.BorrowHistories.Include(b => b.Book).Include(b => b.Customer);
+            return View(borrowHistories.ToList());
+        }
+
+        // GET: BorrowHistories/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BorrowHistory borrowHistory = db.BorrowHistories.Find(id);
+            if (borrowHistory == null)
+            {
+                return HttpNotFound();
+            }
+            return View(borrowHistory);
+        }
+
+        // GET: BorrowHistories/Create
+        public ActionResult Create()
+        {
+            ViewBag.BookId = new SelectList(db.Books, "BookId", "Title");
+            ViewBag.CustomerId = new SelectList(db.Customers, "CustomerId", "Name");
+            return View();
+        }
+
+        // POST: BorrowHistories/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "BorrowHistoryId,BookId,CustomerId,BorrowDate,ReturnDate")] BorrowHistory borrowHistory)
+        {
+            if (ModelState.IsValid)
+            {
+                db.BorrowHistories.Add(borrowHistory);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.BookId = new SelectList(db.Books, "BookId", "Title", borrowHistory.BookId);
+            ViewBag.CustomerId = new SelectList(db.Customers, "CustomerId", "Name", borrowHistory.CustomerId);
+            return View(borrowHistory);
+        }
+
+        // GET: BorrowHistories/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BorrowHistory borrowHistory = db.BorrowHistories.Find(id);
+            if (borrowHistory == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.BookId = new SelectList(db.Books, "BookId", "Title", borrowHistory.BookId);
+            ViewBag.CustomerId = new SelectList(db.Customers, "CustomerId", "Name", borrowHistory.CustomerId);
+            return View(borrowHistory);
+        }
+
+        // POST: BorrowHistories/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "BorrowHistoryId,BookId,CustomerId,BorrowDate,ReturnDate")] BorrowHistory borrowHistory)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(borrowHistory).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.BookId = new SelectList(db.Books, "BookId", "Title", borrowHistory.BookId);
+            ViewBag.CustomerId = new SelectList(db.Customers, "CustomerId", "Name", borrowHistory.CustomerId);
+            return View(borrowHistory);
+        }
+
+        // GET: BorrowHistories/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BorrowHistory borrowHistory = db.BorrowHistories.Find(id);
+            if (borrowHistory == null)
+            {
+                return HttpNotFound();
+            }
+            return View(borrowHistory);
+        }
+
+        // POST: BorrowHistories/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            BorrowHistory borrowHistory = db.BorrowHistories.Find(id);
+            db.BorrowHistories.Remove(borrowHistory);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
