17cc2c65d7f35d567085bc2f11d1794e28518b0a
fun-club <alexlazariuc@outlook.com>
2018-07-27 11:27:48 +0300

    setting display name for SerialNumber property

--- LibraryManagementSystem/Models/Book.cs
+++ LibraryManagementSystem/Models/Book.cs
@@ -12,6 +12,7 @@ namespace LibraryManagementSystem.Models
         [Required]
         public string Title { get; set; }
         [Required]
+        [Display(Name = "ISBN-13")]
         public string SerialNumber { get; set; }
         public string Author { get; set; }
         public string Publisher { get; set; }
